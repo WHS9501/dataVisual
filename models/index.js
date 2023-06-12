f2d8ddbe64b61367a86d9e75511fdcd9e4d43bc1
WHS9501 <wh.seo@pmgrow.co.kr>
2023-06-12 09:13:40 +0900

    🐛 몽고DB connection  option 추가

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 
 const mongoDBconnect = () => {
   mongoose
-    .connect(MONGODB_URL)
+    .connect(MONGODB_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB Connected Successfully"))
     .catch(err => console.log(err));
 };
